db5b27576a5f16be658d10dfe5027a3ce1217b12
Aryan Dahiya <aryandahiya0416@gmail.com>
2026-09-03 11:02:34 +0530

    Delete Question6826 directory

--- Question6826/propfrillfoodapp/propdrillfood/src/components/Home.jsx
+++
@@ -1,7 +0,0 @@
-import Restaurant from './Restaurant'
-
-function Home({ user, setCartCount }) {
-  return <Restaurant user={user}  setCartCount={setCartCount} />
-}
-
-export default Home
--- Question6826/propfrillfoodapp/propdrillfood/src/components/Restaurant.jsx
+++
@@ -1,7 +0,0 @@
-import FoodMenu from './FoodMenu'
-
-function Restaurant({ user, setCartCount }) {
-  return <FoodMenu user={user} setCartCount={setCartCount} />
-}
-
-export default Restaurant
